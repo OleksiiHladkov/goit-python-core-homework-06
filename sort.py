ce1e724635ca1e598664761408af0aa86770aa7f
OleksiiHladkov <131037122+OleksiiHladkov@users.noreply.github.com>
2023-06-13 01:18:59 +0300

    add new functional to sort.py

--- sort.py
+++ sort.py
@@ -1,4 +1,6 @@
 import sys
+import shutil
+import uuid
 from pathlib import Path
 
 
@@ -8,6 +10,8 @@ CATEGORIES = {"images": ["JPEG", "PNG", "JPG", "SVG"],
               "audio": ["MP3", "OGG", "WAV", "AMR"],
               "archives": ["ZIP", "GZ", "TAR"],
               "other": []}
+
+PROCESS_RESULTS = {"known_extentions": set(), "unknown_extentions": set()}
 
 
 def get_category(path: Path) -> str:
@@ -20,6 +24,7 @@ def get_category(path: Path) -> str:
             category = cat
 
     return category
+
 
 def normalized(name: str) -> str:
     CYRILLIC_SYMBOLS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ"
@@ -45,35 +50,99 @@ def normalized(name: str) -> str:
     return translate(name)
 
 
+def write_process_result(key: str, value: str) -> None:
+    new_set = set()
+    res_set = PROCESS_RESULTS.get(key, new_set)
+    res_set.add(value)
+    PROCESS_RESULTS[key] = res_set
+
+
 def moove_file(root_dir: Path, path: Path, category: str) -> None:
     target_dir = root_dir.joinpath(category)
     
     if not target_dir.exists():
         target_dir.mkdir()
     
-    path.replace(target_dir.joinpath(normalized(path.stem) + path.suffix))
+    new_name = f"{normalized(path.stem)}{path.suffix}"
+    new_pass = target_dir.joinpath(new_name)
+    if new_pass.exists():
+        new_name = f"{normalized(path.stem)}_{uuid.uuid4()}{path.suffix}"
+        new_pass = target_dir.joinpath(new_name)
+    
+    path.replace(new_pass)
+
+    write_process_result(category, new_name)
 
 
-def process_files(root_dir: Path):
+def process_files(root_dir: Path) -> None:
     structure = root_dir.glob("**\*")
 
     for path in structure:
         if path.is_file() and path.parent.name not in CATEGORIES:
             category = get_category(path)
+            
+            if category == "other":
+                write_process_result("unknown_extentions", path.suffix)
+            else:
+                write_process_result("known_extentions", path.suffix)
+            
             moove_file(root_dir, path, category)
+
+
+def delete_empty_folders(path: Path, is_root_dir=True) -> None:
+    for child in path.iterdir():
+        if child.stem in CATEGORIES:
+            continue
+        if child.is_dir():
+            delete_empty_folders(child, False)
+        else:
+            return None
+    
+    if not is_root_dir:
+        path.rmdir()
+
+
+def extract_archives(root_dir: Path):
+    arch_cat = "archives"
+    cat_list = CATEGORIES.get(arch_cat)
+    if len(cat_list):
+        cat_str = " ".join(cat_list)
+        pattern = f"{arch_cat}\*.[{cat_str}]*"
+        structure = root_dir.glob(pattern)
+
+        for path in structure:
+            try:
+                shutil.unpack_archive(path, path.with_name(path.stem), path.suffix.replace(".", ""))
+                path.unlink()
+            except shutil.ReadError:
+                print(f"Can't unpack file '{path}'!")
+
+
+def read_process_result():
+    result = ""
+    
+    for key, value in PROCESS_RESULTS.items():
+        text = ", ".join(value).lower()
+        result += f"{key}: {text}\n"
+    
+    print(result)
+
 
 
 def main():
     try:
-        root_dir_str = sys.argv[1]
-        root_dir = Path(root_dir_str)
+        root_dir = Path(sys.argv[1])
     except:
         print(f"You mast enter parameter 'path to folder'. Command example: python {sys.argv[0]} [pathToFolder]")
         return False
     
     if not root_dir.exists():
-        print(f"Path to folder {root_dir_str} is not exists")
+        print(f"Path to folder {sys.argv[1]} is not exists")
+    
     process_files(root_dir)
+    delete_empty_folders(root_dir)
+    extract_archives(root_dir)
+    read_process_result()
 
 
 
